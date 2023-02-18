fb9343461d13c25afa192ca82cdc417d0930b51f
BenB <boyer.benlee@gmail.com>
2023-02-17 22:58:25 -0800

    Added help content

--- PosEr.ps1
+++ PosEr.ps1
@@ -1,34 +1,47 @@
 <#Setup Environment Variables and dir structure if it doesn't exist#>
+<#
+.SYNOPSIS
+This module contains three functions to allow you to easily create and switch between Powershell profile settings.
+It uses the default settings for Powershell and modifies them accordingly, instead of creating multiple profiles.
+It can easily be adapted to create multiple profiles, or modify other profile settings as well.
+#>
 function Set-Environment {
     param (
         [Parameter(
             Mandatory = $true,
             HelpMessage = "Must be a valid directory, default is USERPROFILE\Documents\WindowsPowerShell"
         )]
-        [string] $PowerShellProfilePath
-        )
-        if($null -eq $PowerShellProfilePath){
-            $PowerShellProfilePath = '$env:USERPROFILE\Documents\WindowsPowerShell'
-        }
-        $pkgId = Get-AppPackage Microsoft.WindowsTerminal | Select-Object -ExpandProperty PublisherId
+        [string] $PowerShellProfilePath,
+        [Parameter()]
+        [switch] $h
+    )
 
-        [System.Environment]::SetEnvironmentVariable('PowerShellHome','$PowerShellProfilePath',[System.EnvironmentVariableTarget]::User)
-        [System.Environment]::SetEnvironmentVariable('PowerShellScripts','$PowerShellProfilePath\Scripts',[System.EnvironmentVariableTarget]::User)
-        [System.Environment]::SetEnvironmentVariable('PowerShellPrompts','$PowerShellProfilePath\Prompts',[System.EnvironmentVariableTarget]::User)
-        [System.Environment]::SetEnvironmentVariable('PowerShellVersion',$pkgId,[System.EnvironmentVariableTarget]::User)
-        <#Directories#>
-        mkdir -p $env:PowerShellScripts
-        mkdir -p $env:PowerShellPrompts
-        mkdir -p $env:PowerShellHome'\images'
-        mkdir -p $env:PowerShellHome'\Settings'
+    if($h){
+        Get-Content "$PSScriptRoot\help\environment-help.txt"
+        return
+    }
+    if($null -eq $PowerShellProfilePath){
+        $PowerShellProfilePath = '$env:USERPROFILE\Documents\WindowsPowerShell'
+    }
+    $pkgId = Get-AppPackage Microsoft.WindowsTerminal | Select-Object -ExpandProperty PublisherId
+
+    [System.Environment]::SetEnvironmentVariable('PowerShellHome','$PowerShellProfilePath',[System.EnvironmentVariableTarget]::User)
+    [System.Environment]::SetEnvironmentVariable('PowerShellScripts','$PowerShellProfilePath\Scripts',[System.EnvironmentVariableTarget]::User)
+    [System.Environment]::SetEnvironmentVariable('PowerShellPrompts','$PowerShellProfilePath\Prompts',[System.EnvironmentVariableTarget]::User)
+    [System.Environment]::SetEnvironmentVariable('PowerShellVersion',$pkgId,[System.EnvironmentVariableTarget]::User)
+    <#Directories#>
+    mkdir -p $env:PowerShellScripts
+    mkdir -p $env:PowerShellPrompts
+    mkdir -p $env:PowerShellHome'\Images'
+    mkdir -p $env:PowerShellHome'\Settings'
 }
     
 function Add-Settings {
     param(
-        [ValidateSet('presentation', 'local', 'default')]
+        [ValidateSet('presentation', 'local', 'defaults')]
         [Parameter(
-        Mandatory=$true,
-        HelpMessage="Allowed values: 'Present', 'Local', 'Defaults'"
+            Mandatory=$true,
+            HelpMessage="Allowed values: 'presentation', 'local', 'defaults'"
         )]
         [string] $settingName,
         [Parameter()]
@@ -49,17 +62,17 @@ function Add-Settings {
         [string] $theme,
         [Parameter()]
         [switch] $h
-        )
-
-<#    if($h){
-        Get-Content $($env:PowerShellScripts+'\pse.txt')
-        exit
-    }#>
+    )
+    
+    if($h){
+        Get-Content "$PSScriptRoot\help\settings-help.txt" 
+        return
+    }
     if($null -eq $env:PowerShellHome){
         Set-Environment
     }
     if($null -or '' -eq $backgroundImage){
-        $backgroundImage = $env:PowerShellHome+'\images\background.jpg'
+        $backgroundImage = $env:PowerShellHome+'\Images\background.jpg'
     }
     if($null -or 0 -eq $bgTransparency){
         $bgTransparency = 0.7
@@ -108,30 +121,24 @@ function Add-Settings {
 
     <# necessary Depth level 3, this is subject to change if the powershell settings file obtains further nested values #>
     $SettingsObject | ConvertTo-Json -Depth 100 | Set-Content $outputFile
-
-    <#Properties to query
-    $profile.defaults                                    
-        "backgroundImage":  "PATH",
-        "backgroundImageOpacity":  FLOAT 0-1,
-        "colorScheme":  "Vintage",
-        "font":  {
-                    "face":  "ShureTechMono NF",
-                    "weight":  "medium"
-                },
-        "opacity":  INT 0-100
-    $theme
-        "theme": "light"
-    #>
 }
+
 function Switch-Profile {
     <#Get content from setting path, set content to settings path#>
     param(
         [ValidateSet('presentation', 'local')]
         [Parameter(
-        Mandatory=$true,
-        HelpMessage="Allowed values: 'presentation', 'local'"
+            Mandatory=$true,
+            HelpMessage="Allowed values: 'presentation', 'local'"
         )]
-        [string] $ps
+        [string] $ps,
+        [Parameter()]
+        [switch] $h
     )
+
+    if($h){
+        Get-Content "$PSScriptRoot\help\profiles-help.txt"
+        return
+    }
     Copy-Item -Path "$env:PowerShellHome\Settings\$ps.json" -Destination "$env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminal_$env:PowerShellVersion\LocalState\settings.json"
 }
